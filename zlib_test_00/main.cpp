cfb0061d4cb7507f6a3adfc76f28406ac228cd70
Francesco Biscani <bluescarni@gmail.com>
2016-12-08 14:28:08 +0100

    A couple testing updates.

--- zlib_test_00/main.cpp
+++ zlib_test_00/main.cpp
@@ -1,9 +1,13 @@
 #include <zconf.h>
 #include <zlib.h>
 
+#include <iostream>
+
 int main()
 {
     ::z_stream z;
     (void)z;
+    const char *v = zlibVersion();
+    std::cout << v << '\n';
     return 0;
 }
